89927f6419fdb40abded529722602c09f0c3a094
unknown <cndghks0213@gmail.com>
2021-01-22 04:26:22 +0900

    back_end/views.py 필요없는 부분 삭제

--- back_end/api/views.py
+++ back_end/api/views.py
@@ -1,64 +1,19 @@
-def specific_user(request, username):
-
-    if request.method == 'GET':
-        dbUserData = MongoDbManager().get_users_from_collection({'name': username})
-        responseData = dbUserData[0]
-        del responseData['_id']
-
-        return HttpResponse(json.dumps(responseData), status=200)
-
-    elif request.method == 'POST':
-        try:
-            age, job = request.POST['age'], request.POST['job']
-        except:
-            return HttpResponse(status=400)
-
-        userData = {
-            'name': username,
-            'age': age,
-            'job': job
-        }
-
-        result = MongoDbManager().add_user_on_collection(userData)
-        return HttpResponse(status=201) if result else HttpResponse(status=500)
-
-    elif request.method == 'DELETE':
-        return HttpResponse(status=204)
-
-    else:
-        return HttpResponse(status=405)
-
-
-def all_users(request):
-    if request.method == 'GET':
-        dbUserData = MongoDbManager().get_users_from_collection({})
-        responseData = []
-        for user in dbUserData:
-            del user['_id']
-            responseData.append(user)
-
-        return HttpResponse(json.dumps(responseData), status=200)
-
-    else:
-        return HttpResponse(status=405)
+from django.core.files.storage import default_storage
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from api.models import MongoDbManager
+import gridfs
+import json
 
 @csrf_exempt
 def testapi(request):
 
     if request.method == 'POST':
-        """
         try:
-            print("오긴 옴")
-            data = json.loads(request.body)
+            file = request.FILES['image']
         except:
             return HttpResponse(status=400)
 
-        fruitData = {
-            'fruit' : "사과",
-            'name': data['name'],
-        }
-
-        result = MongoDbManager().add_user_on_collection(fruitData)"""
         file = request.FILES['image']
         file.name = "test.jpg"
         default_storage.save("images" + '/' + file.name, file)
