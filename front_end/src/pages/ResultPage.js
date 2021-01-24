0c514b4b94b5b5251dc22ad512e2c67d107535c9
Chaeyeon Song <codus503@gmail.com>
2021-01-25 05:36:38 +0900

    add Link to button

--- front_end/src/pages/ResultPage.js
+++ front_end/src/pages/ResultPage.js
@@ -39,7 +39,7 @@ const ResultPage = () => {
                 </table>
             </div>
             <div>
-                <div className='gray_button'>취소하기</div>
+                <Link to='/selectupload'><div className='gray_button'>취소하기</div></Link>
                 <Link to='/end'><div className='yellow_button'>결제하기</div></Link>
             </div>
         </div>
