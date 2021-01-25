7edcf3c8e5d23445c138e426da73b6993cba5b2c
DahyeonJeong <snoopy809622@gmail.com>
2021-01-25 10:53:55 +0900

    fix navigator bar style

--- front_end/src/pages/components/TopNavigator.js
+++ front_end/src/pages/components/TopNavigator.js
@@ -5,7 +5,7 @@ const TopNavigator = () => {
     return (
         <div>
             <header>
-               <nav style={{fontSize: '60px'}}>셀프 과일 계산대</nav> 
+               <nav style={{fontSize: '2.25em'}}>셀프 과일 계산대</nav> 
             </header>
         </div>
     );
