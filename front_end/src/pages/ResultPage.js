684e7a208c0faafdd4edde338bfc092962c9f571
DahyeonJeong <snoopy809622@gmail.com>
2021-01-25 15:52:13 +0900

    result page 함수형에서 컴포넌트형으로 변경

--- front_end/src/pages/ResultPage.js
+++ front_end/src/pages/ResultPage.js
@@ -1,29 +1,35 @@
-import React from 'react';
+
+
+import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import './components/Buttons.css';
 import TopNavigator from './components/TopNavigator';
 
-const ResultPage = () => {
-    const q_div = {
-        fontSize: '200%',
-    }
-    const table_div = {
-        textAlign: 'center',
-        border: '1px solid #AAAAAA',
-        width: '60%',
-        display: 'inline-block',
-        borderRadius: '38px'
-    }
-    const img_div = {
-        height: '100%'
-    }
-    return (
-        <div style={{textAlign: 'center'}} >
+
+const q_div = {
+    fontSize: '200%',
+}
+const table_div = {
+    textAlign: 'center',
+    border: '1px solid #AAAAAA',
+    width: '60%',
+    display: 'inline-block',
+    borderRadius: '38px'
+}
+const img_div = {
+    height: '100%'
+}
+
+class ResultPage extends Component {
+    
+    render() {
+        return (
+            <div style={{textAlign: 'center'}} >
             <TopNavigator />
             
             <div style={img_div}>
                 <div style={{fontSize:'200%', padding:'1%'}}>계산결과</div>
-                <img style={{height: '12em'}} src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png'></img>
+                <img style={{height: '12em'}}></img>
             </div>
             <div style={q_div}><span>{}원</span>을 결제하시겠습니까?</div>
             <div style={table_div}>
@@ -51,7 +57,8 @@ const ResultPage = () => {
                 <Link to='/end'><div className='yellow_button'>결제하기</div></Link>
             </div>
         </div>
-    );
-};
+        );
+    }
+}
 
 export default ResultPage;
