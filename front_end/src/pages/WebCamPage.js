17da0b146b023e9b9767c3c4a7e8011c38e3c2f1
이시은 <te04056@naver.com>
2021-01-26 02:44:13 +0900

    Complete Webcam Image Upload Function

--- front_end/src/pages/WebCamPage.js
+++ front_end/src/pages/WebCamPage.js
@@ -9,14 +9,6 @@ const WebCamPage = () => {
     <div style={{ textAlign: "center" }}>
       <TopNavigator />
       <WebcamCapture />
-      <div>
-        <Link to="/selectupload">
-          <div className="gray_button">취소하기</div>
-        </Link>
-        <Link to="/result">
-          <div className="yellow_button">결제하기</div>
-        </Link>
-      </div>
     </div>
   );
 };
--- front_end/src/pages/components/WebcamCapture.js
+++ front_end/src/pages/components/WebcamCapture.js
@@ -1,15 +1,32 @@
 import React from "react";
 import Webcam from "react-webcam";
-
+import {Link} from "react-router-dom";
 
 const WebcamCapture = () => {
-  const webcamRef = React.useRef(null);
+const webcamRef = React.useRef(null);
   const [imgSrc, setImgSrc] = React.useState(null);
+  //const [img, setImage] = useState(null);//추가
 
-  const capture = React.useCallback(() => {
-    const imageSrc = webcamRef.current.getScreenshot();
-    setImgSrc(imageSrc);
-  }, [webcamRef, setImgSrc]);
+  const capture = () => {
+      const imageSrc = webcamRef.current.getScreenshot();
+      setImgSrc(imageSrc);
+      const byteString = atob(imageSrc.split(",")[1]);
+      const ab = new ArrayBuffer(byteString.length);
+      const ia = new Uint8Array(ab);
+      for (let i = 0; i < byteString.length; i++) {
+          ia[i] = byteString.charCodeAt(i);
+      }
+      const blob = new Blob([ia], {
+          type: "image/jpeg"
+      });
+      const file = new File([blob], "image.jpg");
+      console.log(file);
+      const formData = new FormData();
+      formData.append("image", file);
+      const xhr = new XMLHttpRequest();
+      xhr.open('POST', 'http://localhost:8000/api/addimage/');
+      xhr.send(formData);
+  }
 
   const parent_div = {
         width: '100%',
@@ -28,6 +45,14 @@ const WebcamCapture = () => {
               screenshotFormat="image/jpeg"
           />
           </>
+          <div>
+              <Link to="/selectupload">
+                  <div className="gray_button">취소하기</div>
+              </Link>
+              <Link to="/result" onClick={capture}>
+                  <div className="yellow_button" >결제하기</div>
+              </Link>
+          </div>
       </div>
   );
 };
