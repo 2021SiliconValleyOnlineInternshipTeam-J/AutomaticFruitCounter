9045c1bb69f31b5012cee5690cf262a2d70daa30
DahyeonJeong <snoopy809622@gmail.com>
2021-01-25 15:17:53 +0900

    add img div at result page

--- front_end/src/pages/ResultPage.js
+++ front_end/src/pages/ResultPage.js
@@ -5,21 +5,29 @@ import TopNavigator from './components/TopNavigator';
 
 const ResultPage = () => {
     const q_div = {
-        fontSize: '55px',
+        fontSize: '200%',
     }
     const table_div = {
         textAlign: 'center',
         border: '1px solid #AAAAAA',
-        width: '535px',
+        width: '60%',
         display: 'inline-block',
         borderRadius: '38px'
+    }
+    const img_div = {
+        height: '100%'
     }
     return (
         <div style={{textAlign: 'center'}} >
             <TopNavigator />
+            
+            <div style={img_div}>
+                <div style={{fontSize:'200%', padding:'1%'}}>계산결과</div>
+                <img style={{height: '12em'}} src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png'></img>
+            </div>
             <div style={q_div}><span>{}원</span>을 결제하시겠습니까?</div>
             <div style={table_div}>
-                <table style={{fontSize:'20px', display: 'inline-block'}}>
+                <table style={{fontSize:'150%', display: 'inline-block'}}>
                     <thead>
                         <tr>
                             <th>상품명</th>
