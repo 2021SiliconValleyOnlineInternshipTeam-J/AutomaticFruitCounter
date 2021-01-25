a87b159c8c7c625e249f8c1dfa41ec4563370a73
DahyeonJeong <snoopy809622@gmail.com>
2021-01-25 12:22:31 +0900

    fix select page

--- front_end/src/pages/SelectUploadPage.js
+++ front_end/src/pages/SelectUploadPage.js
@@ -6,48 +6,27 @@ import './components/Buttons.css';
 
 
 const SelectUploadPage = ( { history } ) => {
-    const picture_btn = {
+    const select_btn = {
         borderRadius: '12px',
-        fontSize: '45px',
+        fontSize: '300%',
         display: 'inline',
-        padding: '40px 20px',
-        margin: '20px', // top-button, left-right
-        border: '4px solid #FFDD00',
-        color: '#FFE32C',
-    }
-
-    const cam_btn = {
-        borderRadius: '12px',
-        fontSize: '45px',
-        display: 'inline',
-        padding: '40px 20px',
-        margin: '20px',
+        padding: '4% 2%',
+        margin: '3%',
         border: '4px solid #ffe32c7a',
         color: '#ffe32c7a',
     }
 
-    const home_btn = {
-        fontSize: '45px',
-        padding: '20px',
-        margin: '80px',
-        backgroundColor: '#999A9C',
-        color: 'white',
-        borderRadius: '38px',
-        width: '204px',
-        display: 'inline-block'
-    }
-
     const btn_div = {
-        padding:'40px',
+        padding:'3%',
     }
 
     return (
         <div style={{textAlign: 'center', width: '100%'}}>
             <TopNavigator />
-            <div style={{fontSize: '45px', padding: '40px', margin:'20px'}}>계산 방식을 선택해주세요.</div>
+            <div style={{fontSize: '300%', padding: '2%', margin:'3%'}}>계산 방식을 선택해주세요.</div>
             <div>
-                <Link to='/imageupload' style={{ textDecoration: 'none' }}><div style={cam_btn}>사진 업로드</div></Link>
-                <Link to='/webcam' style={{ textDecoration: 'none' }}><div style={cam_btn}>실시간 카메라</div></Link>
+                <Link to='/imageupload' style={{ textDecoration: 'none' }}><div style={select_btn}>사진 업로드</div></Link>
+                <Link to='/webcam' style={{ textDecoration: 'none' }}><div style={select_btn}>실시간 카메라</div></Link>
             </div>
             <div style={btn_div}>
                 <Link to='/'><div className='gray_button'>이전으로</div></Link>
