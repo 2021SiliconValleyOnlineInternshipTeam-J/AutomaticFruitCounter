2b8c5cf68ca2c052e352b5bd658374bd4bc427b1
Chaeyeon Song <codus503@gmail.com>
2021-01-26 13:39:04 +0900

    add result and image in frontend

--- front_end/src/pages/ResultPage.js
+++ front_end/src/pages/ResultPage.js
@@ -1,64 +1,121 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import "./components/Buttons.css";
-import TopNavigator from "./components/TopNavigator";
-
-const q_div = {
-  fontSize: "200%",
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./components/Buttons.css";
+import TopNavigator from "./components/TopNavigator";
+import "./ResultPage.css";
+//style
+const q_div = {
+  fontSize: "200%",
 };
-const table_div = {
-  textAlign: "center",
-  border: "1px solid #AAAAAA",
-  width: "60%",
-  display: "inline-block",
-  borderRadius: "38px",
+const table_div = {
+  textAlign: "center",
+  border: "1px solid #AAAAAA",
+  width: "60%",
+  display: "inline-block",
+  borderRadius: "38px",
 };
-const img_div = {
-  height: "100%",
+const img_div = {
+  height: "100%",
+  imageAlign: "center",
 };
+class FruitField extends React.Component {
+  render() {
+    return (
+      <div >
+        <tr>
+          <td>
+            {this.props.fruit.name}
+          </td>
+          <td>
+            {this.props.fruit.num}
+          </td>
+          <td>
+            {this.props.fruit.total}
+          </td>
+        </tr>
+      </div>
+    );
+  }
+}
+class Table extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {    
+      bill : []
+    }
+  }
 
-class ResultPage extends Component {
-  render() {
-    return (
-      <div style={{ textAlign: "center" }}>
-        <TopNavigator />
-
-        <div style={img_div}>
-          <div style={{ fontSize: "200%", padding: "1%" }}>계산결과</div>
-          <img style={{ height: "12em" }}></img>
-        </div>
-        <div style={q_div}>
-          <span>{}원</span>을 결제하시겠습니까?
-        </div>
-        <div style={table_div}>
-          <table style={{ fontSize: "150%", display: "inline-block" }}>
-            <thead>
-              <tr>
-                <th>상품명</th>
-                <th>수량</th>
-                <th>금액</th>
-              </tr>
-            </thead>
-            <tbody></tbody>
-            <tfoot>
-              <tr>
-                <td colSpan="2">총액</td>
-                <td></td>
-              </tr>
-            </tfoot>
-          </table>
-        </div>
-        <div>
-          <Link to="/selectupload">
-            <div className="gray_button">취소하기</div>
-          </Link>
-          <Link to="/end">
-            <div className="yellow_button">결제하기</div>
-          </Link>
-        </div>
-      </div>
-    );
-  }
+  //json받기
+  //this.setState + axios 이용
+  render() {
+    // key 부여
+    const mapToComponent = data => {
+      return data.map((fruit, i) => {
+        return (<FruitField fruit={fruit} key={i}/>);
+      });
+    };
+    return (
+      <div>
+        <div>{mapToComponent(this.state.bill)}</div>
+      </div>
+    );
+  }
 }
 
-export default ResultPage;
+class ResultPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    image : null,
+    amount : 0,
+    }
+  }
+  //총금액과 이미지 받기
+  //this.setState + axios 이용
+
+  render() {
+    return (
+      <div style={{ textAlign: "center" }}>
+        <TopNavigator />
+        <div style={img_div}>
+          <div style={{ fontSize: "200%", padding: "1%" }}>계산결과</div>
+          <img src = {this.state.image} style={{ height: "12em" }}></img>
+        </div>
+        <div style={q_div}>
+          <span>{this.state.amount}원</span>을 결제하시겠습니까?
+        </div>
+        <div style={table_div}>
+          <table style={{ fontSize: "150%", display: "inline-block" }}>
+            <thead>
+              <tr>
+                <th>상품명</th>
+                <th>수량</th>
+                <th>금액</th>
+              </tr>
+            </thead>
+            <Table/>
+            <tfoot>
+              <tr>
+                <td colSpan="2">총액</td>
+                <td></td>
+                <td className="amount">{this.state.amount}</td>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+        <div>
+          <Link to="/selectupload">
+            <div className="gray_button">취소하기</div>
+          </Link>
+          <Link to="/end">
+            <div className="yellow_button">결제하기</div>
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default ResultPage;
+
