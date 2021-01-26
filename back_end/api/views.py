4b46537f02b35f8dda68cd6f3a7019c44988fd25
unknown <cndghks0213@gmail.com>
2021-01-26 23:01:45 +0900

    입력 이미지를 모델에 넣고, images/output 에 저장

--- back_end/api/views.py
+++ back_end/api/views.py
@@ -1,12 +1,15 @@
 from django.core.files.storage import default_storage
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
-from api.models import MongoDbManager
-# from api.detect import YOLO
-# from api.detect import Bill
+from .models import MongoDbManager
+from .detect import YOLO
+from .detect import Bill
 import gridfs
 import json
 import datetime
+import cv2
+import os
+
 
 @csrf_exempt
 def testapi(request):
@@ -19,12 +22,30 @@ def testapi(request):
 
         file = request.FILES['image']
         now = datetime.datetime.now()
-        default_storage.save("images" + '/input/' + now.strftime('%Y_%m_%d_%H_') + file.name, file)
-        data = {"url": "images" + '/input/' + now.strftime('%Y_%m_%d_%H_') + file.name}
+        file_name = now.strftime('%Y_%m_%d_%H_') + file.name
+        default_storage.save("images" + '/input/' + file_name, file)
+        data = {"url": "images" + '/input/' + now.strftime('%Y_%m_%d_%H_') + file_name}
         result = MongoDbManager().add_user_on_collection(data)
-        
+
         """
             이 부분에 Model image 입력하는 코드 작성
         """
-        
+        WEIGHTS_PATH = "bin/fruit.weights"
+        CFG_PATH = "bin/fruit.cfg"
+        NAMES_PATH = "bin/fruit.names"
+        SRC_PATH = "../images/input/"
+        PRICE_PATH = "bin/fruit.bill"
+        OUTPUT_PATH = "../images/output/output_"
+
+        size_list = [320, 416, 608]
+        SIZE = size_list[2]
+        SCORE_THRESHOLD = 0.1
+        NMS_THRESHOLD = 0.4
+
+        os.chdir("/app/api")
+        src = cv2.imread(SRC_PATH + file_name)
+
+        model = YOLO(WEIGHTS_PATH, CFG_PATH, NAMES_PATH)
+        frame, items = model.detect(frame=src, size=SIZE, score_threshold=SCORE_THRESHOLD, nms_threshold=NMS_THRESHOLD)
+        cv2.imwrite(OUTPUT_PATH + file_name, frame)
         return HttpResponse(status=201) if result else HttpResponse(status=500)
