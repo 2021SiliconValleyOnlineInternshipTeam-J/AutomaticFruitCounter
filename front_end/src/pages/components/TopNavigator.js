f4086ab8afea5f7bca34ff6b00eb579650f2299e
이시은 <te04056@naver.com>
2021-01-27 11:48:29 +0900

    Improving Webcam UI font, title, button

--- front_end/src/pages/components/TopNavigator.js
+++ front_end/src/pages/components/TopNavigator.js
@@ -3,10 +3,12 @@ import './TopNavigator.css';
 
 const TopNavigator = () => {
     return (
-        <div>
-            <header>
-               <nav style={{fontSize: '2.25em'}}>셀프 과일 계산대</nav> 
-            </header>
+        <div class="container">
+            <div>
+                <header>
+                    <nav style={{fontSize: '1.5em'}}>셀프 과일 계산대</nav> 
+                </header>
+            </div>
         </div>
     );
 };
