ec5932d1e3ab0d35dcbcb0b10bd1a3d6ef0fcb0f
DahyeonJeong <snoopy809622@gmail.com>
2021-01-27 21:39:24 +0900

    add link to navigator bar

--- front_end/src/pages/components/TopNavigator.js
+++ front_end/src/pages/components/TopNavigator.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './TopNavigator.css';
+import { Link } from "react-router-dom";
 
 const TopNavigator = () => {
     return (
         <div class="container">
             <div>
                 <header>
-                    <nav style={{fontSize: '1.5em'}}>셀프 과일 계산대</nav> 
+                    <Link to='/' style={{textDecoration: "none", color: "inherit"}}><nav style={{fontSize: '1.5em'}}>셀프 과일 계산대</nav></Link>
                 </header>
             </div>
         </div>
