796c51c9904a0d337e7368a8978c3c0c964b68cc
이시은 <te04056@naver.com>
2021-01-28 16:50:14 +0900

    Adding Logo in Home page, Navigation Bar

--- front_end/src/pages/components/TopNavigator.js
+++ front_end/src/pages/components/TopNavigator.js
@@ -7,7 +7,10 @@ const TopNavigator = () => {
         <div class="container">
             <div>
                 <header>
-                    <Link to='/' style={{textDecoration: "none", color: "inherit"}}><nav style={{fontSize: '1.5em'}}>셀프 과일 계산대</nav></Link>
+                    <Link to='/' style={{textDecoration: "none", color: "inherit", display:"inherit"}}>
+                        <img className="nav_logo" src="https://postfiles.pstatic.net/MjAyMTAxMjhfNyAg/MDAxNjExODE5NzU1Mjc4.JYIR_rf5LYxR3XQU-u7Qubj0G4g_cqbUPihbuzUo_fYg.A-2JVvtwsl_oSrO6JC-Xf05p_xCRoHEDCLqQnTG4A4Eg.PNG.te04056/white_logo.png?type=w773"/>
+                        <nav style={{fontSize: '1.5em'}}>셀프 과일 계산대</nav>
+                    </Link>
                 </header>
             </div>
         </div>
